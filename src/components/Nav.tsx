2f9fff925fd5c3c9bd32fe09ba8df93e5199bb8c
sheben <sheben404@gmail.com>
2020-10-17 14:37:29 +0800

    Money.tsx 的 NotesSection UI

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,6 +5,7 @@ import Icon from './Icon';
 
 
 const NavWrapper = styled.nav`
+ background: white;
  box-shadow: 0 0 3px rgba(0,0,0,0.25);
  line-height: 24px;
  > ul {
--- src/pages/Money.tsx
+++ src/pages/Money.tsx
@@ -26,7 +26,25 @@ const TagsSection = styled.section`
   }
 `
 const NotesSection = styled.section`
-
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label{
+  display:flex;
+  align-items: center;
+    > span{
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+    > input{
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+      padding-left: 10px;
+    }
+  }
 
 `
 const CategorySection = styled.section`
@@ -53,7 +71,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder='在这里添加备注'/>
         </label>
       </NotesSection>
       <CategorySection>
