cc14a078cc65eea0b153e18e73590c09c6a0a05a
sheben <sheben404@gmail.com>
2020-10-17 17:13:43 +0800

    完成标签的选择和新增

--- src/pages/Money.tsx
+++ src/pages/Money.tsx
@@ -14,45 +14,15 @@ const MyLayout = styled(Layout)`
 function Money() {
   return (
     <MyLayout>
-      <TagsSection>
-        <ol>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ol>
-        <button>新增标签</button>
-      </TagsSection>
+      <TagsSection/>
       <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder='在这里添加备注'/>
-        </label>
+
       </NotesSection>
       <CategorySection>
-        <ul>
-          <li className='selected'>支出</li>
-          <li>收入</li>
-        </ul>
+
       </CategorySection>
       <NumberPadSection>
-        <div className='output'>100</div>
-        <div className='pad clearfix'>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>删除</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>清除</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className='ok'>OK</button>
-          <button className='zero'>0</button>
-          <button>.</button>
-        </div>
+
       </NumberPadSection>
     </MyLayout>
   );
