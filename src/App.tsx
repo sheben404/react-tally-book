478015e6b978a06646e12b20175eb70daab08542
sheben <sheben404@gmail.com>
2020-10-20 22:02:02 +0800

    创建 /tags/:tag 路由，学会exact用法

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,27 @@ import Money from './pages/Money';
 import Statistics from './pages/Statistics';
 import NoMatch from './pages/NoMatch';
 import styled from 'styled-components';
+import {Tag} from './pages/Tag';
 
 const AppWrapper = styled.div`
  color: #333;
-`
+`;
 
 function App() {
   return (
     <AppWrapper>
       <Router>
         <Switch>
-          <Route path="/tags">
+          <Route exact path="/tags">
             <Tags/>
           </Route>
-          <Route path="/money">
+          <Route exact path="/tags/:tag">
+            <Tag/>
+          </Route>
+          <Route exact path="/money">
             <Money/>
           </Route>
-          <Route path="/statistics">
+          <Route exact path="/statistics">
             <Statistics/>
           </Route>
           <Redirect exact from='/' to='/money'/>
--- src/pages/Tags.tsx
+++ src/pages/Tags.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {useTags} from 'useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import {Link} from 'react-router-dom';
 
 const TagList = styled.ol`
   font-size: 16px;
@@ -11,13 +12,15 @@ const TagList = styled.ol`
     //#e5e5e7
     border-bottom: 1px solid #d5d5d5;
     line-height: 20px;
-    padding: 12px 16px 12px 0;
     margin-left: 16px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    > a{
+      padding: 12px 16px 12px 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
   }
-`
+`;
 const Button = styled.button`
   font-size: 18px;
   border: none;
@@ -25,16 +28,17 @@ const Button = styled.button`
   border-radius: 4px;
   background: #767676;
   color: white;
-`
+`;
 const Center = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-`
+`;
 const Space = styled.div`
   height: 16px;
-`
+`;
+
 function Tags() {
   const {tags, setTags} = useTags();
   return (
@@ -42,8 +46,10 @@ function Tags() {
       <TagList>
         {tags.map(tag =>
           <li key={tag}>
-            <span className="oneLine">{tag}</span>
-            <Icon name="right"/>
+            <Link to={'/tags/' + tag}>
+              <span className="oneLine">{tag}</span>
+              <Icon name="right"/>
+            </Link>
           </li>
         )}
       </TagList>
