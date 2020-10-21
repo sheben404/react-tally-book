097b2763e5f42c8c11606bbc8508bdf80775bab3
sheben <sheben404@gmail.com>
2020-10-21 19:52:37 +0800

    tags数据持久化

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,15 +1,24 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {createId} from './lib/createId';
-
-const defaultTags = [
-  {id: createId(), name: '衣'},
-  {id: createId(), name: '食'},
-  {id: createId(), name: '住'},
-  {id: createId(), name: '行'}
-];
+import {useUpdate} from './hooks/useUpdate';
 
 const useTags = () => { //封装一个自定义Hook
-  const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+  const [tags, setTags] = useState<{ id: number; name: string }[]>([]);
+  useEffect(() => {
+    let localTags = JSON.parse(window.localStorage.getItem('tags') || '[]');
+    if (localTags.length === 0) {
+      localTags = [
+        {id: createId(), name: '衣'},
+        {id: createId(), name: '食'},
+        {id: createId(), name: '住'},
+        {id: createId(), name: '行'}
+      ];
+    }
+    setTags(localTags);
+  }, []);
+  useUpdate(() => {
+    window.localStorage.setItem('tags', JSON.stringify(tags));
+  }, [tags]);
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
   const findTagIndex = (id: number) => {
     let result = -1;
@@ -22,11 +31,21 @@ const useTags = () => { //封装一个自定义Hook
     return result;
   };
   const updateTag = (id: number, {name}: { name: string }) => {
-    setTags(tags.map (tag => tag.id === id ? {id,name} : tag))
+    setTags(tags.map(tag => tag.id === id ? {id, name} : tag));
   };
   const deleteTag = (id: number) => {
-    setTags(tags.filter(tag => tag.id !==id))
-  }
-  return {tags, setTags, findTag,updateTag,findTagIndex,deleteTag};
+    setTags(tags.filter(tag => tag.id !== id));
+    window.alert('删除成功');
+    window.history.back();
+  };
+  const addTag = () => {
+    const tagName = window.prompt('新增标签的名称为：');
+    if (tagName === '') window.alert('标签名不能为空');
+    if (tagName !== null && tagName !== '') {
+      setTags([...tags, {id: createId(), name: tagName}]);
+      window.alert('创建成功');
+    }
+  };
+  return {tags, addTag, setTags, findTag, updateTag, findTagIndex, deleteTag};
 };
 export {useTags};
