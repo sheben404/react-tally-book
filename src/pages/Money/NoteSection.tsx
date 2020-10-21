52a539b6e2257a9975c9eb73dd3fc5448a3be8cf
sheben <sheben404@gmail.com>
2020-10-21 09:30:44 +0800

    Tag.tsx的样式完成

--- src/pages/Money/NoteSection.tsx
+++ src/pages/Money/NoteSection.tsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
-import React, {ChangeEventHandler, useRef} from 'react';
+import React, {ChangeEventHandler} from 'react';
 import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
-  padding: 0 16px;
+  padding: 14px 16px;
   font-size: 14px;
 `
 type Props = {
@@ -13,13 +13,14 @@ type Props = {
 }
 const NoteSection: React.FC<Props> = (props) =>{
   const note = props.value
-  const refInput = useRef<HTMLInputElement>(null)
   const onChange:ChangeEventHandler<HTMLInputElement> = (e) => {
       props.onChange(e.target.value)
   }
   return (
     <Wrapper>
-        <Input type='text' label='备注' value={note} onChange={onChange}/>
+        <Input type='text' label='备注' value={note} onChange={onChange}
+               placeholder="请填写备注"
+        />
     </Wrapper>
   )
 }
--- src/pages/Tag.tsx
+++ src/pages/Tag.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {useTags} from 'useTags';
-import {useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom';
 import Layout from 'components/Layout';
 import Icon from '../components/Icon';
 import {Button} from '../components/Button';
 import styled from 'styled-components';
+import {Input} from '../components/Input';
+import {Center} from '../components/Center';
+import {Space} from '../components/Space';
 
 type Params = {
   id: string
@@ -16,31 +19,39 @@ const Topbar = styled.header`
   line-height: 20px;
   padding: 14px;
   background: white;
-`
+`;
+const InputWrapper = styled.div`
+  background: white;
+  padding: 0 16px;
+  margin-top: 8px;
+`;
 const Tag: React.FC = () => {
-  const {findTag} = useTags()
-  let {id} = useParams<Params>()
+  const {findTag} = useTags();
+  let {id} = useParams<Params>();
 
-  const tag = findTag(parseInt(id))
-  return(
+  const tag = findTag(parseInt(id));
+  return (
     <Layout>
       <Topbar>
         <Icon name='left'/>
         <span>编辑标签</span>
         <Icon/>
       </Topbar>
-      <div>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder='标签名'/>
-        </label>
-      </div>
-      <div>
-        <Button>删除标签</Button>
-      </div>
+      <InputWrapper>
+        <Input label="标签名" type="text" placeholder="标签名" value={tag.name}/>
+      </InputWrapper>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
+        <div>
+          <Button>删除标签</Button>
+        </div>
+        <Space/>
+      </Center>
     </Layout>
-  )
-}
-export {Tag}
+  );
+};
+export {Tag};
 
 
