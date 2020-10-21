fe39d374445cf9942ff11ece57ca0c13561620e3
sheben <sheben404@gmail.com>
2020-10-21 10:30:54 +0800

    编辑tag名

--- src/pages/Tag.tsx
+++ src/pages/Tag.tsx
@@ -26,7 +26,7 @@ const InputWrapper = styled.div`
   margin-top: 8px;
 `;
 const Tag: React.FC = () => {
-  const {findTag} = useTags();
+  const {findTag,updateTag} = useTags();
   let {id} = useParams<Params>();
 
   const tag = findTag(parseInt(id));
@@ -38,7 +38,12 @@ const Tag: React.FC = () => {
         <Icon/>
       </Topbar>
       <InputWrapper>
-        <Input label="标签名" type="text" placeholder="标签名" value={tag.name}/>
+        <Input label="标签名" type="text" placeholder="标签名"
+               value={tag.name}
+               onChange={e => {
+                 updateTag(tag.id,{name:e.target.value})
+               }}
+        />
       </InputWrapper>
       <Center>
         <Space/>
