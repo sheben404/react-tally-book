a02b1e2fa47744f65b069f325080ca4be81c6a13
sheben <sheben404@gmail.com>
2020-10-22 16:35:48 +0800

    build

---
+++ build/precache-manifest.454059e10baeb8f3e03f49b5f5f81091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f630cc86cacaac771221b910e3ebe2",
+    "url": "/react-tally-book/build/index.html"
+  },
+  {
+    "revision": "709dac0cfaad0288fa5c",
+    "url": "/react-tally-book/build/static/css/main.22e2ebde.chunk.css"
+  },
+  {
+    "revision": "32557528cc46fecdc7b7",
+    "url": "/react-tally-book/build/static/js/2.8f2769c9.chunk.js"
+  },
+  {
+    "revision": "de7c95d42723c2d4f76d99ad9a65a751",
+    "url": "/react-tally-book/build/static/js/2.8f2769c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "709dac0cfaad0288fa5c",
+    "url": "/react-tally-book/build/static/js/main.9b8f2483.chunk.js"
+  },
+  {
+    "revision": "0d719dac7af23098c533",
+    "url": "/react-tally-book/build/static/js/runtime-main.1e686f78.js"
+  }
+]);
